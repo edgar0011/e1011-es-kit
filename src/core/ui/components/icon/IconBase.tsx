5f70034907e1f2b6ae231fcf8161b69ae7dcfbd7
Martin Weiser <martin.weiser@gmail.com>
2023-03-05 13:58:34 +0100

    feat: IconBase can have inner content, text, svg....

--- src/core/ui/components/icon/IconBase.tsx
+++ src/core/ui/components/icon/IconBase.tsx
@@ -8,29 +8,33 @@ type IconBaseType = PropsWithChildren<any> & {
   minHeight?: string
   width?: string
   height?: string
+  size?: string
   color?: string
   className?: string
 }
 
 export const IconBase = memo<IconBaseType>(({
-  iconUrl, minWidth = '1rem', minHeight = '1rem', width, height, color = 'currentColor', className,
+  iconUrl, minWidth = '1rem', minHeight = '1rem', size, width, height, color = 'currentColor', className = '', children,
 }: IconBaseType) => {
   const styles = useMemo(() => (
     {
       '--min-width': minWidth,
       '--min-height': minHeight,
-      '--width': width,
-      '--height': height,
-      '--icon-url': `url(${iconUrl})`,
-      '--icon-color': color,
+      '--width': size || width,
+      '--height': size || height,
+      ...(size ? { fontSize: size } : {}),
+      ...(iconUrl ? { '--icon-url': `url(${iconUrl})` } : {}),
+      ...(iconUrl ? { '--icon-color': color } : { '--icon-content-color': color }),
     }
-  ), [minWidth, minHeight, width, height, iconUrl, color])
+  ), [minWidth, minHeight, size, width, height, iconUrl, color])
 
   return (
     <span
       className={`${(classes as any)['icon-base']} icon-base ${className}`}
       style={styles as CSSProperties}
-    />
+    >
+      {(!iconUrl && children) && children}
+    </span>
   )
 })
 
