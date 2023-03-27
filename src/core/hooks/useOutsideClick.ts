692c93da9ae031a537f10460e597438725589654
Martin Weiser <martin.weiser@gmail.com>
2023-03-28 01:20:21 +0200

    feat: adds outsideClickHandler

--- src/core/hooks/useOutsideClick.ts
+++ src/core/hooks/useOutsideClick.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useCallback } from 'react'
 
 import { EventName } from '../constants/ui.constants'
 import { handleClickOutside as defaultHandleClickOutside } from '../ui/utils/clickOutside'
@@ -10,12 +10,27 @@ export function useOutsideClick(
   element: Element,
   callback?: CallbackFunction,
   handleClickOutside = defaultHandleClickOutside(element, callback),
-): void {
+): () => void {
+  const unsubscriber = useCallback(() => document.removeEventListener(EventName.Click, handleClickOutside), [])
+
   useEffect(() => {
+    unsubscriber()
     document.addEventListener(EventName.Click, handleClickOutside)
 
-    return () => {
-      document.removeEventListener(EventName.Click, handleClickOutside)
-    }
-  }, [element, handleClickOutside])
+    return unsubscriber
+  }, [element, handleClickOutside, unsubscriber])
+
+  return unsubscriber
+}
+
+export function outsideClickHandler(
+  element: Element,
+  callback?: CallbackFunction,
+  handleClickOutside = defaultHandleClickOutside(element, callback),
+): () => void {
+  const unsubscriber = () => document.removeEventListener(EventName.Click, handleClickOutside)
+
+  document.addEventListener(EventName.Click, handleClickOutside)
+
+  return unsubscriber
 }
