fbb2e2a6110da33c276649e0dd833e687b44ec3d
Martin Weiser <martin.weiser@gmail.com>
2024-03-04 14:31:45 +0100

    chore: cleanup

--- src/core/ui/types/CommonProps.ts
+++ src/core/ui/types/CommonProps.ts
@@ -1,13 +1,6 @@
-import { Ref, RefObject } from 'react'
-
 export type CommonProps = {
   id?: string
   'data-testid'?: string
   dataTestId?: string
   className?: string
-}
-
-
-export type ForwardedRefContainer = {
-  forwardedRef?: RefObject<HTMLDivElement> | Ref<HTMLDivElement> | null
 }
