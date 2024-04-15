49a6888bb1cb93369cf5b44edd69611f421db81c
Martin Weiser <martin.weiser@gmail.com>
2024-04-15 21:34:31 +0200

    feat: new ui helpers, utils

--- src/core/utils/helpers/ui.spec.ts
+++ src/core/utils/helpers/ui.spec.ts
@@ -1,5 +1,8 @@
-import { classNames } from './ui'
+/**
+ * @jest-environment jsdom
+ */
 
+import { anchorClick, classNames, generateId, noop } from './ui'
 
 describe('classNames', () => {
   it('should have proper classNames string', () => {
@@ -13,5 +16,59 @@ describe('classNames', () => {
     )
 
     expect(classes).toBe('my-class other-class some-other-class2 external-class external-class2')
+  })
+
+  it('should test generateId', () => {
+    const generatedIds = [
+      generateId('button'),
+      generateId('button'),
+      generateId('button'),
+      generateId('button'),
+      generateId('button'),
+    ]
+
+    expect(generatedIds).toEqual(['button0', 'button1', 'button2', 'button3', 'button4'])
+  })
+
+  it('should test anchroClick', async () => {
+    anchorClick('https://www.google.com', '_blank')
+
+    expect(anchorClick.aElement).toBeDefined()
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const spyClick = jest.spyOn(anchorClick.aElement, 'click')
+
+    anchorClick('https://www.google.com', '_blank', true)
+
+    expect(spyClick).toHaveBeenCalled()
+
+    expect(anchorClick.aElement).toBeNull()
+  })
+
+  it('should test noop', async () => {
+    const event = {
+      preventDefault: jest.fn(),
+      stopPropagation: jest.fn(),
+      stopImmediatePropagation: jest.fn(),
+    }
+
+    const customEvent = new CustomEvent('ui-test-event')
+
+    noop(event)
+
+    expect(event.preventDefault).toHaveBeenCalled()
+    expect(event.stopPropagation).toHaveBeenCalled()
+    expect(event.stopImmediatePropagation).toHaveBeenCalled()
+
+    const spyPreventDefault = jest.spyOn(customEvent, 'preventDefault')
+    const spyStopPropagation = jest.spyOn(customEvent, 'stopPropagation')
+    const spyStopImmediatePropagation = jest.spyOn(customEvent, 'stopImmediatePropagation')
+
+    noop(customEvent)
+
+    expect(spyPreventDefault).toHaveBeenCalled()
+    expect(spyStopPropagation).toHaveBeenCalled()
+    expect(spyStopImmediatePropagation).toHaveBeenCalled()
   })
 })
--- src/core/utils/helpers/ui.ts
+++ src/core/utils/helpers/ui.ts
@@ -1,6 +1,3 @@
-export const noop = (): void => undefined
-
-
 export const mapSerReplacer
 = (key: string, value: unknown): unknown | { dataType: string; value: Array<unknown>} => {
   if (value instanceof Map) {
@@ -35,4 +32,75 @@ export const parseProps = (props: Record<string, unknown>):PropsCategoriesType =
     }
   })
   return { dataProps, restProps }
+}
+
+
+
+type GeneratorIdCallable = (token: string, increment?: boolean, forcedValue?: number | undefined) => string
+type GenerateId = { tokens?: Record<string, number | null | undefined> } & GeneratorIdCallable
+
+export const generateId: GenerateId = (
+  token: string,
+  increment = true,
+  forcedValue: number | undefined = undefined,
+) => {
+  generateId.tokens = generateId.tokens || {}
+
+  if (forcedValue !== undefined && forcedValue !== null) {
+    generateId.tokens[token] = forcedValue
+    return `${token}${generateId?.tokens?.[token] as number}`
+  }
+
+  const noValue = generateId?.tokens[token] === undefined || generateId?.tokens[token] === null
+
+  generateId.tokens[token] = generateId.tokens[token] || 0
+
+  if (noValue) {
+    return `${token}${0}`
+  }
+
+  if (increment) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    generateId.tokens[token] += 1
+
+    return `${token}${generateId?.tokens?.[token] as number}`
+  }
+
+  return `${token}${generateId?.tokens?.[token] as number}`
+}
+
+type FAnchorClick = { aElement?: HTMLAnchorElement | null }
+  & ((href: string, target?: string, remove?: boolean) => void)
+
+export const anchorClick: FAnchorClick = (href: string, target = '_top', remove = false): HTMLAnchorElement | null => {
+  anchorClick.aElement = anchorClick.aElement || document.createElement('a')
+
+  const { aElement } = anchorClick
+
+  aElement.setAttribute('href', href)
+  aElement.setAttribute('target', target)
+
+  aElement.dataset.date = `${Date.now()}`
+  aElement.click()
+  if (remove) {
+    aElement.remove()
+    anchorClick.aElement = null
+  }
+
+  return aElement
+}
+
+
+
+export type NoopEvent = {
+  preventDefault?: () => void
+  stopPropagation?: () => void
+  stopImmediatePropagation?: () => void
+} | Event
+
+export const noop = (event?: NoopEvent): void => {
+  event?.preventDefault?.()
+  event?.stopPropagation?.()
+  event?.stopImmediatePropagation?.()
 }
