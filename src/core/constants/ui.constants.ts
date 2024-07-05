118ef83fe88c1b7faac9fd1b8a99fccdb1571265
Martin Weiser <martin.weiser@gmail.com>
2024-07-05 17:31:59 +0200

    chore: upgrades deps

--- src/core/constants/ui.constants.ts
+++ src/core/constants/ui.constants.ts
@@ -8,3 +8,17 @@ export enum EventName {
   KeyDown = 'keydown',
   Scroll = 'scroll',
 }
+
+export enum KeyCode {
+  UpArrow = 38,
+  RightArrow = 39,
+  DownArrow = 40,
+  LeftArrow = 37,
+  PageUp = 33,
+  PageDown = 34,
+  Home = 36,
+  End = 35,
+  Enter = 13,
+  Space = 32,
+  Escape = 27,
+}
