7425c20e374258e8497aef3b2b922041a51f575c
Martin Weiser <martin.weiser@gmail.com>
2024-07-22 14:12:08 +0200

    chore: upgrades deps

--- src/core/utils/helpers/date.ts
+++ src/core/utils/helpers/date.ts
@@ -1,6 +1,13 @@
 import dayjs from 'dayjs'
 
 export const getDate = (): string => new Date().toString()
-export const DATE_FORMAT = 'MM-DD-YYYY HH-mm'
+
 
 export const formatDateToTimestamp = (date = new Date()): string => dayjs(date).format('YYYY-MM-DD HH:mm:ss')
+
+export const DATE_FORMAT = 'MM-DD-YYYY'
+export const DATE_TIME_FORMAT = 'MM-DD-YYYY HH-mm-ss'
+
+export const formatDate = (date: string | Date): string => dayjs(date).format(DATE_FORMAT)
+export const formatDateTime = (date: string | Date): string => dayjs(date).format(DATE_TIME_FORMAT)
+
